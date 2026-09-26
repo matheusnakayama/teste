f30ea25793e11454f9498af5654506b6ad16cc4c
matheushideo <114515802+matheusnakayama@users.noreply.github.com>
2026-09-25 21:13:08 -0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -32,7 +32,7 @@ export interface RoomChatMessage {
   isSystem?: boolean;
 }
 
-export type RoomModerationAction = 'kick' | 'mute' | 'unmute' | 'promote';
+export type RoomModerationAction = 'kick' | 'mute' | 'unmute' | 'promote' | 'epstein';
 
 export interface RoomModerationEvent {
   action: RoomModerationAction;
